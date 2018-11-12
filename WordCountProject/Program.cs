b37fb4619df4f6e055f5c6703fc3cf16d6d5d177
Rina Lingad <pixie.czar@gmail.com>
2018-11-13 03:30:46 +1300

    Add WordCount project

---
+++ WordCountProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharpExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wordCounter = new WordCounter();
+
+            wordCounter.ParseText(args[0]);
+
+            wordCounter.PrintSorted();
+        }
+    }
+}
---
+++ WordCountProject/WordCounter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpExercises
+{
+    class WordCounter
+    {
+        private Dictionary<string, int> _dictWords;
+
+        public WordCounter()
+        {
+            _dictWords = new Dictionary<string, int>();
+        }
+
+        public void ParseText(string filePath)
+        {
+            // TODO: Error checking
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+
+            foreach (string line in lines)
+            {
+                var words = line.Split(new[] { ' ', '.', ',', '-' });
+                foreach (var word in words)
+                {
+                    if (_dictWords.ContainsKey(word))
+                    {
+                        _dictWords[word]++;
+                    }
+                    else
+                    {
+                        _dictWords.Add(word, 1);
+                    }
+                }
+            }
+        }
+
+        public void PrintSorted()
+        {
+            var list = _dictWords.OrderByDescending(x => x.Value).ToList();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.Key + " " + item.Value.ToString());
+            }
+            Console.ReadLine();
+        }
+
+        public void PrintCount()
+        {
+            foreach (var item in _dictWords)
+            {
+                Console.WriteLine(item.Key + " " + item.Value.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
