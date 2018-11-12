4e2b6da4a62b4e1b835b91219cfcbd7e037e7bc9
Rina Lingad <pixie.czar@gmail.com>
2018-11-13 03:31:05 +1300

    Initial implementation of BowlingScoreCounter

---
+++ BowlingScoreProject/BowlingScoreCounter.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingScoreProject
+{
+    enum BonusRollType
+    {
+        None,
+        AddRollAfter,
+        AddSecondRollAfter
+    }
+
+    class BowlingScoreCounter
+    {
+        private int _score;
+        private string[] _frameRolls;
+        private BonusRollType[,] _bonusRollTypePerFrame;
+
+        public BowlingScoreCounter()
+        {
+            _score = 0;
+            _bonusRollTypePerFrame = new BonusRollType[,] {
+                { BonusRollType.None, BonusRollType.None },
+                { BonusRollType.None, BonusRollType.None },
+                { BonusRollType.None, BonusRollType.None },
+                { BonusRollType.None, BonusRollType.None },
+                { BonusRollType.None, BonusRollType.None },
+                { BonusRollType.None, BonusRollType.None },
+                { BonusRollType.None, BonusRollType.None },
+                { BonusRollType.None, BonusRollType.None },
+                { BonusRollType.None, BonusRollType.None },
+                { BonusRollType.None, BonusRollType.None },
+            };
+        }
+
+        public void ComputeFromInput()
+        {
+            Console.WriteLine("Enter pins knocked down per roll per frame separated by space");
+            for (int frame = 0; frame < 10; frame++)
+            {
+                Console.WriteLine("Pins knocked down for frame " + frame);
+                var frameInput = Console.ReadLine();
+                _frameRolls = frameInput.Split(' ');
+
+                switch (_frameRolls.Length)
+                {
+                    case 1:
+                        // Assume correct input (10). TODO: Error checking
+                        ProcessStrike(frame);
+                        break;
+                    case 2:
+                        ProcessSpareOrOther(frame);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void ProcessStrike(int frame)
+        {
+            _score += 10;
+            _bonusRollTypePerFrame[frame, 0] = BonusRollType.AddRollAfter;
+            _bonusRollTypePerFrame[frame, 1] = BonusRollType.AddSecondRollAfter;
+
+            if ((frame-1 >= 0) &&
+                (_bonusRollTypePerFrame[frame-1, 0] == BonusRollType.AddRollAfter))
+            {
+                _score += 10;
+                _bonusRollTypePerFrame[frame-1, 0] = BonusRollType.None;
+            }
+
+            if ((frame-2 >= 0) &&
+                (_bonusRollTypePerFrame[frame-2, 0] == BonusRollType.AddSecondRollAfter))
+            {
+                _score += 10;
+                _bonusRollTypePerFrame[frame-2, 0] = BonusRollType.None;
+            }
+        }
+
+        private void ProcessSpareOrOther(int frame)
+        {
+            int firstRoll = Convert.ToInt32(_frameRolls[0]);
+            int secondRoll = Convert.ToInt32(_frameRolls[1]);
+
+            _score = _score + firstRoll + secondRoll;
+
+            if (firstRoll + secondRoll == 10)
+                _bonusRollTypePerFrame[frame, 0] = BonusRollType.AddRollAfter;
+
+            if ((frame-1 >= 0) &&
+                (_bonusRollTypePerFrame[frame-1, 0] == BonusRollType.AddRollAfter))
+            {
+                _score += firstRoll;
+                _bonusRollTypePerFrame[frame-1, 0] = BonusRollType.None;
+            }
+
+            if ((frame-2 >= 0) &&
+                (_bonusRollTypePerFrame[frame-2, 0] == BonusRollType.AddSecondRollAfter))
+            {
+                _score += secondRoll;
+                _bonusRollTypePerFrame[frame-2, 0] = BonusRollType.None;
+            }
+
+            if ((frame-2 >= 0) &&
+                (_bonusRollTypePerFrame[frame-2, 0] == BonusRollType.AddSecondRollAfter))
+            {
+                _score += firstRoll;
+                _bonusRollTypePerFrame[frame-2, 0] = BonusRollType.None;
+            }
+        }
+
+        public void PrintTotal()
+        {
+            Console.WriteLine("Total score is " + _score.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BowlingScoreProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BowlingScoreProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BowlingScoreCounter bowlingScoreCounter = new BowlingScoreCounter();
+
+            // Get input per frame
+            bowlingScoreCounter.ComputeFromInput();
+
+            // Print output per frame
+            bowlingScoreCounter.PrintTotal();
+        }
+    }
+}
