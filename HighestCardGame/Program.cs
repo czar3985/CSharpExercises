27232ddcf081661d0d07fa97aff9092f7185ee6f
Rina Lingad <pixie.czar@gmail.com>
2018-11-17 21:35:20 +1300

    Highest Card Game project initial commit

---
+++ HighestCardGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HighestCardGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
