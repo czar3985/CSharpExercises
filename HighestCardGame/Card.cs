48273c720c5e83adc31564989fb41066477aab61
Rina Lingad <pixie.czar@gmail.com>
2018-11-18 18:38:15 +1300

    Add Card class and unit test

---
+++ HighestCardGame/Card.cs
@@ -0,0 +1,63 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("HighestCardGameUnitTests")]
+namespace HighestCardGame
+{
+    public enum Suit
+    {
+        Club = 1,
+        Diamond,
+        Heart,
+        Spade
+    }
+
+    public enum CardNumber
+    {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+    }
+
+    class Card
+    {
+        const int numOfSuits = 4;
+
+        public string Name { get; set; }
+        public int Rank { get; set; }
+
+        private CardNumber _cardNumber;
+        private Suit _suit;
+
+        public Card(CardNumber cardNumber, Suit suit)
+        {
+            _cardNumber = cardNumber;
+            _suit = suit;
+
+            Name = DetermineCardName();
+            Rank = DetermineCardRank();
+        }
+
+        private string DetermineCardName()
+        {
+            return _cardNumber.ToString() + _suit.ToString();
+        }
+
+        private int DetermineCardRank()
+        {
+            // Ranking implemented: from high to low:
+            // ace, king, queen, jack, 10, 9, 8, 7, 6, 5, 4, 3, 2
+            // spades(highest), hearts, diamonds, clubs (lowest)
+            return ((int)_cardNumber * numOfSuits) + (int)_suit;
+        }
+    }
+}
---
+++ HighestCardGameUnitTests/CardTests.cs
@@ -0,0 +1,74 @@
+﻿using HighestCardGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace HighestCardGameUnitTests
+{
+    [TestClass]
+    public class CardTests
+    {
+        [DataTestMethod]
+        [DataRow(CardNumber.Two, Suit.Club, "TwoClub", 1)]
+        [DataRow(CardNumber.Two, Suit.Diamond, "TwoDiamond", 2)]
+        [DataRow(CardNumber.Two, Suit.Heart, "TwoHeart", 3)]
+        [DataRow(CardNumber.Two, Suit.Spade, "TwoSpade", 4)]
+        [DataRow(CardNumber.Three, Suit.Club, "ThreeClub", 5)]
+        [DataRow(CardNumber.Three, Suit.Diamond, "ThreeDiamond", 6)]
+        [DataRow(CardNumber.Three, Suit.Heart, "ThreeHeart", 7)]
+        [DataRow(CardNumber.Three, Suit.Spade, "ThreeSpade", 8)]
+        [DataRow(CardNumber.Four, Suit.Club, "FourClub", 9)]
+        [DataRow(CardNumber.Four, Suit.Diamond, "FourDiamond", 10)]
+        [DataRow(CardNumber.Four, Suit.Heart, "FourHeart", 11)]
+        [DataRow(CardNumber.Four, Suit.Spade, "FourSpade", 12)]
+        [DataRow(CardNumber.Five, Suit.Club, "FiveClub", 13)]
+        [DataRow(CardNumber.Five, Suit.Diamond, "FiveDiamond", 14)]
+        [DataRow(CardNumber.Five, Suit.Heart, "FiveHeart", 15)]
+        [DataRow(CardNumber.Five, Suit.Spade, "FiveSpade", 16)]
+        [DataRow(CardNumber.Six, Suit.Club, "SixClub", 17)]
+        [DataRow(CardNumber.Six, Suit.Diamond, "SixDiamond", 18)]
+        [DataRow(CardNumber.Six, Suit.Heart, "SixHeart", 19)]
+        [DataRow(CardNumber.Six, Suit.Spade, "SixSpade", 20)]
+        [DataRow(CardNumber.Seven, Suit.Club, "SevenClub", 21)]
+        [DataRow(CardNumber.Seven, Suit.Diamond, "SevenDiamond", 22)]
+        [DataRow(CardNumber.Seven, Suit.Heart, "SevenHeart", 23)]
+        [DataRow(CardNumber.Seven, Suit.Spade, "SevenSpade", 24)]
+        [DataRow(CardNumber.Eight, Suit.Club, "EightClub", 25)]
+        [DataRow(CardNumber.Eight, Suit.Diamond, "EightDiamond", 26)]
+        [DataRow(CardNumber.Eight, Suit.Heart, "EightHeart", 27)]
+        [DataRow(CardNumber.Eight, Suit.Spade, "EightSpade", 28)]
+        [DataRow(CardNumber.Nine, Suit.Club, "NineClub", 29)]
+        [DataRow(CardNumber.Nine, Suit.Diamond, "NineDiamond", 30)]
+        [DataRow(CardNumber.Nine, Suit.Heart, "NineHeart", 31)]
+        [DataRow(CardNumber.Nine, Suit.Spade, "NineSpade", 32)]
+        [DataRow(CardNumber.Ten, Suit.Club, "TenClub", 33)]
+        [DataRow(CardNumber.Ten, Suit.Diamond, "TenDiamond", 34)]
+        [DataRow(CardNumber.Ten, Suit.Heart, "TenHeart", 35)]
+        [DataRow(CardNumber.Ten, Suit.Spade, "TenSpade", 36)]
+        [DataRow(CardNumber.Jack, Suit.Club, "JackClub", 37)]
+        [DataRow(CardNumber.Jack, Suit.Diamond, "JackDiamond", 38)]
+        [DataRow(CardNumber.Jack, Suit.Heart, "JackHeart", 39)]
+        [DataRow(CardNumber.Jack, Suit.Spade, "JackSpade", 40)]
+        [DataRow(CardNumber.Queen, Suit.Club, "QueenClub", 41)]
+        [DataRow(CardNumber.Queen, Suit.Diamond, "QueenDiamond", 42)]
+        [DataRow(CardNumber.Queen, Suit.Heart, "QueenHeart", 43)]
+        [DataRow(CardNumber.Queen, Suit.Spade, "QueenSpade", 44)]
+        [DataRow(CardNumber.King, Suit.Club, "KingClub", 45)]
+        [DataRow(CardNumber.King, Suit.Diamond, "KingDiamond", 46)]
+        [DataRow(CardNumber.King, Suit.Heart, "KingHeart", 47)]
+        [DataRow(CardNumber.King, Suit.Spade, "KingSpade", 48)]
+        [DataRow(CardNumber.Ace, Suit.Club, "AceClub", 49)]
+        [DataRow(CardNumber.Ace, Suit.Diamond, "AceDiamond", 50)]
+        [DataRow(CardNumber.Ace, Suit.Heart, "AceHeart", 51)]
+        [DataRow(CardNumber.Ace, Suit.Spade, "AceSpade", 52)]
+        public void CardPropertyTest(
+            CardNumber cardNumber,
+            Suit suit,
+            string expectedName,
+            int expectedRank)
+        {
+            Card card = new Card(cardNumber, suit);
+            Assert.AreEqual(expectedRank, card.Rank);
+            Assert.AreEqual(expectedName, card.Name);
+        }
+    }
+}
